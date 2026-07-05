da280b0f25e5deb84583137a9291c26661d754eb
varsha25-main <khushibarnwal5722@gmail.com>
2026-07-05 21:41:40 +0530

    Refactor simple calculator with improved user interface
    Updated calculator functions and added print statements for user guidance.

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,10 +1,10 @@
-def add(num1,num2):
+def add(num1,num2): 
     return (num1 + num2)
 
 def subtract(num1,num2):
     return (num1 - num2)
 
-def product(num1,num2):
+def multiply(num1,num2):
     return (num1 * num2)
 
 def divide(num1,num2):
@@ -12,29 +12,30 @@ def divide(num1,num2):
         print("cannot divide by zero\n")
     else:
         return (num1/num2)
-
+print("----------SIMPLE CALCULATOR----------\n")
+print("Performs basic operations ('ADD, SUBTRACT, MULTIPLY, DIVIDE') on two user-input numbers")
 def simple_calculator():
     value=True
     while value:
         try:
-            num1=float(input("enter 1st number="))
-            num2=float(input("enter 2nd number="))
-            opt=input("enter operation you want to perform (+,-,*,/) :")
+            num1=float(input("\nEnter 1st number="))
+            num2=float(input("\nEnter 2nd number="))
+            opt=input("\nEnter operation you want to perform (+,-,*,/) :")
             if opt == "+":
                 result = add(num1,num2)
             elif opt == "-":
                 result = subtract(num1,num2)
             elif opt == "*":
-                result = product(num1,num2)
+                result = multiply(num1,num2)
             elif opt == "/":
                 result = divide(num1,num2)
             else:
-                print("Invalid operator please choose valid operrations (+,-,*,/)")
+                print("\nInvalid operator please choose valid operrations (+,-,*,/)")
             print(f"{num1} {opt} {num2} = {result}")
         except ValueError:
-            print("Invalid input")
+            print("\nInvalid input")
         
-        choice=input("Do you want to perform more calculations (yes/no) :")
+        choice=input("\nDo you want to perform more calculations (yes/no) :")
         if choice.lower() == "no":
             value = False
             break
